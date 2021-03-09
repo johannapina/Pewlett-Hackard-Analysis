351984081149cdea96d09819a8d90d368f21808f
Johanna Pina <johannapina@yahoo.com>
2021-03-09 16:16:24 -0500

    updated queries file with new queries and uploaded retirement_info file

--- queries.sql
+++ queries.sql
@@ -33,12 +33,13 @@ CREATE TABLE salaries (
   PRIMARY KEY (emp_no)
 );
 CREATE TABLE Dept_Emp (
-  dept_no VARCHAR NOT NULL,
   emp_no INT NOT NULL,
+  dept_no VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (dept_no)
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
 );
 CREATE TABLE Titles (
   emp_no INT NOT NULL,
@@ -48,3 +49,28 @@ CREATE TABLE Titles (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 SELECT * FROM departments;
+SELECT * FROM employees;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+SELECT * FROM dept_manager;
+SELECT * FROM dept_emp;
+DROP TABLE Dept_Emp CASCADE;
+SELECT COUNT(*)
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Create new table
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- View created table
+SELECT * FROM retirement_info;
